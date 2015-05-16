cdd2ac3c90af8f9c02adaa49393bc79e33ee45da
Jon Collins <jon@jon-collins.com>
2015-05-16 19:47:01 +0100

    Refactored javascript. Added todo.txt.

--- js/viewer.js
+++ js/viewer.js
@@ -33,26 +33,10 @@ VIEWER.Approver = function (id, name, status, annotations) {
     this.annotations = annotations || [];
 };
 
-VIEWER.Approver.prototype.setStatus = function (status) {
-    "use strict";
-
-    this.status = status;
-};
-
 VIEWER.Approver.prototype.addAnnotation = function (annotation) {
 	"use strict";
 	
 	this.annotations[this.annotations.length] = annotation;
-};
-
-VIEWER.toggleAnnotations = function (checked, approverId) {
-	var searchName = "[id^=div_" + approverId + "_]"; 
-	$(searchName).each(function () {
-		if(checked)
-			$(this).show();
-		else
-			$(this).hide();
-	  });
 };
 
 VIEWER.renderApproverList = function (approversList) {
@@ -79,6 +63,12 @@ VIEWER.setCurrentApproverStatus = function (status) {
     $("#approversList").html(VIEWER.renderApproverList(VIEWER.approvers));
 };
 
+VIEWER.Approver.prototype.setStatus = function (status) {
+    "use strict";
+
+    this.status = status;
+};
+
 VIEWER.toggleAnnotateTool = function(caller){
 	"use strict";
 	VIEWER.createAnnotations = !VIEWER.createAnnotations;
@@ -86,6 +76,10 @@ VIEWER.toggleAnnotateTool = function(caller){
 		$(caller).addClass("active");
 	else
 		$(caller).removeClass("active");
+};
+
+VIEWER.toggleAnnotations = function (checked, approverId) {
+	$("[id^=div_" + approverId + "_]").toggle(checked);
 };
 
 $(document).ready(function () {
